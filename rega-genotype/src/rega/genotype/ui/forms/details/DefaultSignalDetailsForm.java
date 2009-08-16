e5ce8a730d9146562f40e21c406169d5c74b96b1
plibin0 <none>
2009-08-16 21:20:32 +0200

    svn revision 2061 svn date 2008-10-13 16:29:46 list phylo signal items correctly

--- rega-genotype/src/rega/genotype/ui/forms/details/DefaultSignalDetailsForm.java
+++ rega-genotype/src/rega/genotype/ui/forms/details/DefaultSignalDetailsForm.java
@@ -2,20 +2,25 @@ package rega.genotype.ui.forms.details;
 
 import java.io.File;
 
+import net.sf.witty.wt.WContainerWidget;
 import net.sf.witty.wt.WText;
 import net.sf.witty.wt.i8n.WMessage;
 import rega.genotype.ui.data.OrganismDefinition;
 import rega.genotype.ui.data.SaxParser;
 import rega.genotype.ui.forms.IDetailsForm;
+import rega.genotype.ui.framework.widgets.WListContainerWidget;
 import rega.genotype.ui.util.GenotypeLib;
 
 public class DefaultSignalDetailsForm extends IDetailsForm {
 
 	@Override
 	public void fillForm(SaxParser p, OrganismDefinition od, File jobDir) {
-		addWidget(new WText(tr("defaultSignalAnalysis.signalValue")));
-		addWidget(new WText(lt(p.getValue("genotype_result.sequence.result[pure-puzzle].signal"))));
-		addWidget(new WText(tr("defaultSignalAnalysis.signalComment")));
+		WListContainerWidget ul = new WListContainerWidget(this);
+		WContainerWidget li;
+		li = ul.addItem(new WText(tr("defaultSignalAnalysis.signalValue")));
+		li.addWidget(new WText(lt(p.getValue("genotype_result.sequence.result[pure-puzzle].signal"))));
+		li = ul.addItem(new WText(tr("defaultSignalAnalysis.signalComment")));
+		
 		addWidget(GenotypeLib.getWImageFromFile(GenotypeLib.getSignalPNG(GenotypeLib.getFile(jobDir, p.getValue("genotype_result.sequence.result[pure-puzzle].puzzle")))));
 	}
 	
