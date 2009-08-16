ab1db83fd2bb77d67582e8a964d534ec8b58a0a0
plibin0 <none>
2009-08-16 21:16:45 +0200

    svn revision 1970 svn date 2008-10-01 18:25:45 recombination pdf

--- rega-genotype/src/rega/genotype/ui/recombination/RecombinationPlot.java
+++ rega-genotype/src/rega/genotype/ui/recombination/RecombinationPlot.java
@@ -1,5 +1,7 @@
 package rega.genotype.ui.recombination;
 
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -17,6 +19,14 @@ import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYItemRenderer;
 
 import rega.genotype.ui.util.GenotypeLib;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Rectangle;
+import com.lowagie.text.pdf.DefaultFontMapper;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfTemplate;
+import com.lowagie.text.pdf.PdfWriter;
 
 public class RecombinationPlot {
 	public static JFreeChart getRecombinationPlot(File dataCsv) throws FileNotFoundException, UnsupportedEncodingException {
@@ -51,6 +61,39 @@ public class RecombinationPlot {
 		return pngFile;
 	}
 	
+	public static File getRecombinationPDF(File jobDir, int sequenceIndex, String type) throws IOException {
+		File pdfFile = new File(jobDir.getAbsolutePath() + File.separatorChar + "plot_" + sequenceIndex + "_" + type + ".pdf");
+		
+		if(!pdfFile.exists()) {
+			FileOutputStream fos = new FileOutputStream(pdfFile);
+			int width = 720;
+			int height = 450;
+			
+			Rectangle pagesize = new Rectangle(width, height);
+			Document document = new Document(pagesize, 50, 50, 50, 50);
+			try {
+				PdfWriter writer = PdfWriter.getInstance(document, fos);
+				document.addAuthor("Rega Genotype Tool");
+				document.addSubject("Recombination plot");
+				document.open();
+				PdfContentByte cb = writer.getDirectContent();
+				PdfTemplate tp = cb.createTemplate(width, height);
+				Graphics2D g2 = tp.createGraphics(width, height, new DefaultFontMapper());
+				Rectangle2D r2D = new Rectangle2D.Double(0, 0, width, height);
+				File csvFile = GenotypeLib.getCsvData(jobDir, sequenceIndex, type);
+				getRecombinationPlot(csvFile).draw(g2, r2D, null);
+				g2.dispose();
+				cb.addTemplate(tp, 0, 0);
+			} catch (DocumentException de) {
+				System.err.println(de.getMessage());
+			}
+			document.close();
+		}
+		
+		return pdfFile;
+	}
+
+	
 	public static void main(String[] args) throws IOException {
 		JFrame f = new JFrame();
 		ChartPanel chartPanel = new ChartPanel(getRecombinationPlot(new File("/home/plibin0/projects/utrecht/recombination/data.csv")));
@@ -59,5 +102,6 @@ public class RecombinationPlot {
 		f.setVisible(true);
 		
 		getRecombinationPNG(new File("/home/plibin0/projects/subtypetool/genomePng"), 0, "pure");
+		getRecombinationPDF(new File("/home/plibin0/projects/subtypetool/genomePng"), 0, "pure");
 	}
 }
