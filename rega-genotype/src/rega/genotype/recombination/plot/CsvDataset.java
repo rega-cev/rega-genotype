2ddb7c3cc8ebaa66102933e5033658a04d68c6ec
plibin0 <none>
2009-08-16 21:15:30 +0200

    svn revision 1942 svn date 2008-09-29 15:30:10 recombination plot

---
+++ rega-genotype/src/rega/genotype/recombination/plot/CsvDataset.java
@@ -0,0 +1,55 @@
+package rega.genotype.recombination.plot;
+
+import net.sf.regadb.csv.Table;
+
+import org.jfree.data.DomainOrder;
+import org.jfree.data.xy.AbstractXYDataset;
+
+public class CsvDataset extends AbstractXYDataset {
+	private Table t_;
+
+	public CsvDataset(Table t) {
+		t_ = t;
+	}
+
+	@Override
+	public DomainOrder getDomainOrder() {
+		return DomainOrder.ASCENDING;
+	}
+
+	@Override
+	public int getItemCount(int arg0) {
+		return t_.numRows() - 1;
+	}
+
+	@Override
+	public Number getX(int arg0, int arg1) {
+		return Double.parseDouble(t_.valueAt(0, arg1 + 1));
+	}
+
+	@Override
+	public double getXValue(int arg0, int arg1) {
+		return Double.parseDouble(t_.valueAt(0, arg1 + 1));
+	}
+
+	@Override
+	public Number getY(int arg0, int arg1) {
+		return Double.parseDouble(t_.valueAt(arg0 + 1, arg1 + 1));
+	}
+
+	@Override
+	public double getYValue(int arg0, int arg1) {
+		return Double.parseDouble(t_.valueAt(arg0 + 1, arg1 + 1));
+	}
+
+	@Override
+	public int getSeriesCount() {
+		return t_.numColumns() - 3;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Comparable getSeriesKey(int arg0) {
+		return t_.valueAt(arg0 + 1, 0);
+	}
+}
