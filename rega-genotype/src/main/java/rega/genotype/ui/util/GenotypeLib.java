561acb88c28c1abd5afaee6772101033ee798e35
Koen Deforche <koen@emweb.be>
2015-06-09 09:35:39 +0200

    print convert command

--- rega-genotype/src/main/java/rega/genotype/ui/util/GenotypeLib.java
+++ rega-genotype/src/main/java/rega/genotype/ui/util/GenotypeLib.java
@@ -109,6 +109,8 @@ public class GenotypeLib {
 	}
 	
 	private static void imageMagickConvert(File in, File out) throws IOException, InterruptedException {
+		System.err.println("Running: " + Settings.getInstance().getImageMagickConvertCmd() + " " + in.getAbsolutePath() + " " + out.getAbsolutePath());
+
 		Process ps = new ProcessBuilder(Settings.getInstance().getImageMagickConvertCmd(), in.getAbsolutePath(), out.getAbsolutePath()).start();
 		
 		StreamReaderThread stdout = new StreamReaderThread(ps.getInputStream(), System.out, "image magick stdout: ");
