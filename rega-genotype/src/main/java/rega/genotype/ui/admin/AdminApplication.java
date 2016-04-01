69fb35c1510dc829b9b6e560cb0a64fa4a095439
Michael <michael@emweb.be>
2016-04-01 15:37:00 +0200

    cleanup

--- rega-genotype/src/main/java/rega/genotype/ui/admin/AdminApplication.java
+++ rega-genotype/src/main/java/rega/genotype/ui/admin/AdminApplication.java
@@ -1,26 +1,16 @@
 package rega.genotype.ui.admin;
 
-import java.util.EnumSet;
-import java.util.List;
-
-import jdk.nashorn.internal.ir.SetSplitState;
 import rega.genotype.config.Config;
 import rega.genotype.utils.Settings;
-import eu.webtoolkit.jwt.DomElement;
 import eu.webtoolkit.jwt.Signal;
-import eu.webtoolkit.jwt.ValidationStyleFlag;
 import eu.webtoolkit.jwt.WApplication;
-import eu.webtoolkit.jwt.WCssStyleSheet;
 import eu.webtoolkit.jwt.WCssTheme;
 import eu.webtoolkit.jwt.WEnvironment;
 import eu.webtoolkit.jwt.WLineEdit;
 import eu.webtoolkit.jwt.WLink;
 import eu.webtoolkit.jwt.WPushButton;
 import eu.webtoolkit.jwt.WText;
-import eu.webtoolkit.jwt.WTheme;
-import eu.webtoolkit.jwt.WWidget;
 import eu.webtoolkit.jwt.WXmlLocalizedStrings;
-import eu.webtoolkit.jwt.WValidator.Result;
 
 /**
  * Determine what widget to show in admin area.
