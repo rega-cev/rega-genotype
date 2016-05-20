9710e77c1bf0eb63528feb6101aad08277dadc52
Michael <michael@emweb.be>
2016-05-20 15:07:11 +0200

    FileEditor

---
+++ rega-genotype/src/main/java/rega/genotype/ui/admin/file_editor/FileEditor.java
@@ -0,0 +1,99 @@
+package rega.genotype.ui.admin.file_editor;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import rega.genotype.ui.util.GenotypeLib;
+import rega.genotype.utils.FileUtil;
+import rega.genotype.utils.Settings;
+import eu.webtoolkit.jwt.Signal1;
+import eu.webtoolkit.jwt.WTabWidget;
+
+/**
+ * Coordinate between simple and smart file editors -> make sure 
+ * that they show the same data.
+ * All the files are saved in a tmp work dir and copied back to 
+ * tool dir when save is clicked. 
+ * 
+ * @author michael
+ */
+public class FileEditor extends WTabWidget {
+	private SimpleFileEditorView simpleFileEditor;
+	private SmartFileEditor smartFileEditor;
+	private File workDir; // tmp dir with tool copy for coordination between smart and simple editors. 
+	private File toolDir;
+
+	public FileEditor(File toolDir) {
+		
+		this.toolDir = toolDir;
+		workDir = GenotypeLib.createJobDir(
+				Settings.getInstance().getBaseJobDir() + File.separator + "file_editor");
+
+		try {
+			FileUtil.copyDirContentRecorsively(toolDir, workDir);
+		} catch (IOException e) {
+			e.printStackTrace();
+			assert(false);
+		}
+
+		// view
+
+		simpleFileEditor = new SimpleFileEditorView(workDir);
+		smartFileEditor = new SmartFileEditor(workDir);
+
+		addTab(smartFileEditor, "Tool editor");
+		addTab(simpleFileEditor, "Simple file editor");
+
+		currentChanged().addListener(this, new Signal1.Listener<Integer>() {
+			public void trigger(Integer arg) {
+				if (getCurrentWidget().equals(smartFileEditor)) {// simple -> smart
+					smartFileEditor.rereadFiles();
+				} else { // smart -> simple
+					smartFileEditor.saveAll();
+					simpleFileEditor.rereadFiles();
+				}
+			}
+		});
+
+		smartFileEditor.editingInnerXmlElement().addListener(this, new Signal1.Listener<Integer>() {
+			public void trigger(Integer arg) {
+				setTabEnabled(1, arg == 1);
+			}
+		});
+	}
+
+	public SimpleFileEditorView getSimpleFileEditor() {
+		return simpleFileEditor;
+	}
+
+	public SmartFileEditor getSmartFileEditor() {
+		return smartFileEditor;
+	}
+
+	public void setReadOnly(boolean isReadOnly) {
+		simpleFileEditor.setReadOnly(isReadOnly);
+		smartFileEditor.setDisabled(isReadOnly);
+	}
+
+	public boolean saveAll() {
+		if (getCurrentWidget().equals(smartFileEditor)) { // most resent changes are in sent changes are in smart
+			smartFileEditor.saveAll();
+			simpleFileEditor.rereadFiles();// only the simple editor contains all the files
+		}
+
+		simpleFileEditor.saveAll();
+
+		// copy the changes back to tool dir.
+		try {
+			FileUtils.deleteDirectory(toolDir);
+			FileUtils.moveDirectory(workDir, toolDir);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
+	}
+}
