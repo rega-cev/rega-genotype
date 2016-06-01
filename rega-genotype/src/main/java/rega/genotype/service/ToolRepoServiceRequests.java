96bb7eeb01f4193e0c3f33e5cb27602888b3eb6a
Michael <michael@emweb.be>
2016-06-01 18:07:00 +0200

    resurrect NoV test

--- rega-genotype/src/main/java/rega/genotype/service/ToolRepoServiceRequests.java
+++ rega-genotype/src/main/java/rega/genotype/service/ToolRepoServiceRequests.java
@@ -8,11 +8,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.net.UnknownHostException;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
