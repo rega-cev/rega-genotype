fe98c8470c7563d0e229ae5d93ad6e4e747fbe73
Michael <michael@emweb.be>
2016-06-01 10:07:30 +0200

    PenViralGenotypeAnalysisTest add more tests

--- rega-genotype/src/test/java/rega/genotype/blast/PenViralGenotypeAnalysisTest.java
+++ rega-genotype/src/test/java/rega/genotype/blast/PenViralGenotypeAnalysisTest.java
@@ -9,6 +9,10 @@ import junit.framework.TestCase;
 
 import org.junit.Test;
 
+import rega.genotype.AlignmentAnalyses;
+import rega.genotype.AlignmentAnalyses.Cluster;
+import rega.genotype.BlastAnalysis;
+import rega.genotype.BlastAnalysis.Result;
 import rega.genotype.FileFormatException;
 import rega.genotype.ParameterProblemException;
 import rega.genotype.config.Config.ToolConfig;
@@ -18,11 +22,18 @@ import rega.genotype.tools.blast.BlastTool;
 import rega.genotype.ui.utils.TestUtils;
 import rega.genotype.utils.FileUtil;
 
+/**
+ * Tests for the pen-viral tool (blast tool)
+ * 
+ * Note: the test is using xmls from base-unit-test-work-dir/xml/pen-viral1
+ * 
+ * @author michael
+ */
 public class PenViralGenotypeAnalysisTest extends TestCase{
-private String fasta;
-    
-    private List<File> jobDirs = new ArrayList<File>();
-    
+	private String fasta;
+	private List<File> jobDirs = new ArrayList<File>();
+	private final double DELTA = 0.01;
+
 	protected void setUp() {	
 		fasta = ">2I_AF100469_Mexico_1992\n" + 
 				"TCCAGGCTTTACCATAATGGCCGCAATCCTGGCATACACCATAGGAACGACGCATTTCCAAAGAGTCC" + 
@@ -47,6 +58,7 @@ private String fasta;
        	File jobDir = TestUtils.setup(fasta);
     	jobDirs.add(jobDir);
 
+    	//tool data is in base-unit-test-work-dir/xml/pen-viral1
     	ToolConfig toolConfig = Settings.getInstance().getConfig().getToolConfigById("pen-viral", "1");
     	BlastTool blastTool;
 		try {
@@ -68,7 +80,19 @@ private String fasta;
 			@Override
 			public void endSequence() {
 				String assignment = getValue("/genotype_result/sequence/conclusion/assigned/id");
+
+				String similarityStr = getValue("/genotype_result/sequence/result[@id='blast']/cluster/absolute-similarity");
+				String absoluteStr = getValue("/genotype_result/sequence/result[@id='blast']/cluster/absolute-score");
+				String relativeStr = getValue("/genotype_result/sequence/result[@id='blast']/cluster/relative-score");
+
+				double similarity = Double.parseDouble(similarityStr);
+				double absolute = Double.parseDouble(absoluteStr);
+				double relative = Double.parseDouble(relativeStr);
+
 				assertEquals("Flaviviridae Flavivirus Dengue virus", assignment);
+				assertTrue(Math.abs(absolute - 981.0) < DELTA);
+				assertTrue(Math.abs(relative - 4.9296484) < DELTA);
+				assertTrue(Math.abs(similarity - 91.31016) < DELTA);
 			}
 
 			@Override
@@ -79,6 +103,43 @@ private String fasta;
     	p.parseFile(jobDir);
     }
 
+	@Test
+	public void testBlast() {
+		File jobDir = TestUtils.setup(fasta);
+    	jobDirs.add(jobDir);
+
+    	//tool data is in base-unit-test-work-dir/xml/pen-viral1
+    	ToolConfig toolConfig = Settings.getInstance().getConfig().getToolConfigById("pen-viral", "1");
+		
+		AlignmentAnalyses alignmentAnalyses = null;
+		try {
+			File blastFile = new File(toolConfig.getConfiguration(), "blast.xml");
+			jobDir.mkdirs();
+			alignmentAnalyses = new AlignmentAnalyses(blastFile, null, jobDir);
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("could not create AlignmentAnalyses.");
+		} catch (ParameterProblemException e) {
+			e.printStackTrace();
+			fail("could not create AlignmentAnalyses.");
+		} catch (FileFormatException e) {
+			e.printStackTrace();
+			fail("could not create AlignmentAnalyses.");
+		}
+		BlastAnalysis blastAnalysis = (BlastAnalysis) alignmentAnalyses.getAnalysis("blast");
+		blastAnalysis.setDetailsOptions(null); // this part does not change the computation.
+
+		// test max cutoff
+		blastAnalysis.setAbsCutoff(1000.0);
+		List<Result> analysisResults = blastAnalysis.analyze(alignmentAnalyses, fasta);
+		assertTrue(analysisResults.size() == 1);
+
+		Result result = analysisResults.get(0);
+
+		assertTrue(result.getAbsScore() == -1);
+		assertEquals(result.getConcludedCluster(), null);
+	}
+	
     @Test
     public void testBenchmark() {
     	boolean doTestBenchmark = false;
@@ -101,7 +162,7 @@ private String fasta;
 					TestUtils.getResultFile(jobDir).getAbsolutePath());
 
 	    	System.err.println("Full analysis time in ms: " + (System.currentTimeMillis() - startTime));
-			// assertTrue((System.currentTimeMillis() - startTime) < 7000); // can change on different computers. 
+			assertTrue((System.currentTimeMillis() - startTime) < 7000); // can change on different computers. 
 		} catch (IOException e1) {
 			e1.printStackTrace();
 			fail("IOException occured during analysis runtime");
