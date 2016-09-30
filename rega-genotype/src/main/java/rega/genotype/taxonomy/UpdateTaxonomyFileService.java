c610abf2ba5f9049bab3253660138681e29babf5
Michael <michael@emweb.be>
2016-09-30 09:31:11 +0200

    UpdateTaxonomyFileService: use wget and not java url connection

--- rega-genotype/src/main/java/rega/genotype/taxonomy/UpdateTaxonomyFileService.java
+++ rega-genotype/src/main/java/rega/genotype/taxonomy/UpdateTaxonomyFileService.java
@@ -1,13 +1,6 @@
 package rega.genotype.taxonomy;
-
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-
-import org.apache.commons.io.IOUtils;
 
 import rega.genotype.singletons.Settings;
 
@@ -20,27 +13,30 @@ import rega.genotype.singletons.Settings;
 public class UpdateTaxonomyFileService {
 	private static String TAXONOMY_URL = "http://www.uniprot.org/taxonomy/?query=Viruses&format=tab";
 	private static String TAXONOMY_FILE_NAME = "taxonomy.tab";
-	
+
 	public static File download() {
-		URLConnection connection;
 		File taxonomyFile = taxonomyFile();
+
+		taxonomyFile.delete();
+
+		Process fetchFasta = null;
 		try {
-			connection = new URL(TAXONOMY_URL).openConnection();
-			connection.setRequestProperty("Content-Type", "multipart/form-data"); // Allow to add a file
+			String cmd = "wget '" + TAXONOMY_URL + "' -O " + taxonomyFile.getAbsolutePath();
+			String[] shellCmd = {"/bin/sh", "-c", cmd};
+			System.err.println(shellCmd);
+			fetchFasta = Runtime.getRuntime().exec(shellCmd);
 
-			FileWriter w = new FileWriter(taxonomyFile);
-			IOUtils.copy(connection.getInputStream(), w);
-
-		} catch (MalformedURLException e) {
+			int exitResult = fetchFasta.waitFor();
+			if (exitResult != 0)
+				return null;
+		} catch (IOException e) {
 			e.printStackTrace();
 			return null;
-		} catch (IOException e) {
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 			return null;
 		}
 
-		System.err.println("Taxonomy file downloaded.");
-		
 		return taxonomyFile;
 	}
 
