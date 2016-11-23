24d2ef3cc0fbe2ff2dd9c1b29cdb90457efc11a1
Michael <michael@emweb.be>
2016-11-23 11:10:54 +0100

    BlastUtil computeBestRefSeq make sure that all the process buffers are read

--- rega-genotype/src/main/java/rega/genotype/utils/BlastUtil.java
+++ rega-genotype/src/main/java/rega/genotype/utils/BlastUtil.java
@@ -41,47 +41,51 @@ public class BlastUtil {
 	public static void computeBestRefSeq(AbstractSequence sequence, File workDir, File out, File blastdb)
 			throws ApplicationException, IOException, InterruptedException {
 
-        Process blast = null;
-        String blastPath = Settings.getInstance().getBlastPathStr();
+		Process blast = null;
+		String blastPath = Settings.getInstance().getBlastPathStr();
 
-        if (sequence.getLength() != 0) {
-        	File query = new File(workDir,"query.fasta");
-        	FileOutputStream queryFile = new FileOutputStream(query);
-        	sequence.writeFastaOutput(queryFile);
-        	queryFile.close();
+		if (sequence.getLength() != 0) {
+			File query = new File(workDir,"query.fasta");
+			FileOutputStream queryFile = new FileOutputStream(query);
+			sequence.writeFastaOutput(queryFile);
+			queryFile.close();
 
-        	File db = new File(workDir, "db.fasta");
-        	String cmd = blastPath + "blastall -p blastn -q -1 -r 1 " 
-        			+ " -i " + query.getAbsolutePath()
-        			+ " -m 8 -d " + blastdb.getAbsolutePath();
+			String cmd = blastPath + "blastall -p blastn -q -1 -r 1 " 
+					+ " -i " + query.getAbsolutePath()
+					+ " -m 8 -d " + blastdb.getAbsolutePath();
 
-        	System.err.println(cmd);
+			System.err.println(cmd);
 
-        	blast = Runtime.getRuntime().exec(cmd, null, workDir);
-        	InputStream inputStream = blast.getInputStream();
+			blast = Runtime.getRuntime().exec(cmd, null, workDir);
+			InputStream inputStream = blast.getInputStream();
 
-        	final LineNumberReader reader = new LineNumberReader(new InputStreamReader(inputStream));
-        	String line = reader.readLine();
-        	if (line == null)
-        		throw new ApplicationException("blast results are empty.");
-        	String[] values = line.split("\t");
+			final LineNumberReader inReader = new LineNumberReader(new InputStreamReader(inputStream));
+			String inLine = inReader.readLine();
+			if (inLine == null)
+				throw new ApplicationException("blast results are empty.");
+			String[] values = inLine.split("\t");
 
-        	int exitResult = blast.waitFor();
+			final BufferedReader errReader = new BufferedReader(new InputStreamReader(blast.getErrorStream()));
+			String errLine;
+			while ((errLine = errReader.readLine()) != null)
+				System.out.println(errLine);
 
-        	blast.getErrorStream().close();
-        	blast.getInputStream().close();
-        	blast.getOutputStream().close();
+			int exitResult = blast.waitFor();
 
-        	if (exitResult != 0)
-        		throw new ApplicationException("blast exited with error: " + exitResult);
+			blast.getErrorStream().close();
+			blast.getInputStream().close();
+			blast.getOutputStream().close();
+
+			if (exitResult != 0)
+				throw new ApplicationException("blast exited with error: " + exitResult);
 
 			if (values.length != 12)
 				throw new ApplicationException("blast result format error");
 
 			findSequence(values[BlastAnalysis.BLAST_RESULT_SUBJECT_ID_IDX],
 					blastdb, out);
-        }
-    }
+		}
+	}
 
 	public static void findSequence(String sequenceId, File blastdb, File out) throws IOException, ApplicationException, InterruptedException {
 		out.getParentFile().mkdirs();
