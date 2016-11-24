36763ba52944b63fda1a56e07efe7c332828698c
Michael <michael@emweb.be>
2016-11-24 11:55:57 +0100

    BlastUtil: print process err

--- rega-genotype/src/main/java/rega/genotype/utils/BlastUtil.java
+++ rega-genotype/src/main/java/rega/genotype/utils/BlastUtil.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.LineNumberReader;
 
 import rega.genotype.AbstractSequence;
 import rega.genotype.ApplicationException;
@@ -57,13 +55,15 @@ public class BlastUtil {
 			System.err.println(cmd);
 
 			blast = Runtime.getRuntime().exec(cmd, null, workDir);
-			InputStream inputStream = blast.getInputStream();
 
-			final LineNumberReader inReader = new LineNumberReader(new InputStreamReader(inputStream));
+			BufferedReader inReader = new BufferedReader(new InputStreamReader(blast.getInputStream()));
 			String inLine = inReader.readLine();
 			if (inLine == null)
 				throw new ApplicationException("blast results are empty.");
 			String[] values = inLine.split("\t");
+
+			while ((inLine = inReader.readLine()) != null) // clear in buff so java dont dead lock
+				System.out.println(inLine);
 
 			final BufferedReader errReader = new BufferedReader(new InputStreamReader(blast.getErrorStream()));
 			String errLine;
