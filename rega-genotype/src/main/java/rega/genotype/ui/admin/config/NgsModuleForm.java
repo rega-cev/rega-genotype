3bf35f2695974d530b15a07607a61c8c1334c4c6
Michael <michael@emweb.be>
2016-12-05 17:50:34 +0100

    NgsModuleForm: createNgsDatabases if they are not created.

--- rega-genotype/src/main/java/rega/genotype/ui/admin/config/NgsModuleForm.java
+++ rega-genotype/src/main/java/rega/genotype/ui/admin/config/NgsModuleForm.java
@@ -11,24 +11,25 @@ import rega.genotype.python.PythonEnv;
 import rega.genotype.singletons.Settings;
 import rega.genotype.taxonomy.RegaSystemFiles;
 import rega.genotype.ui.framework.widgets.FormTemplate;
+import rega.genotype.ui.framework.widgets.StandardDialog;
 import rega.genotype.ui.util.FileUpload;
 import rega.genotype.utils.StreamReaderRuntime;
 import eu.webtoolkit.jwt.Signal;
 import eu.webtoolkit.jwt.Signal1;
+import eu.webtoolkit.jwt.WDialog.DialogCode;
 import eu.webtoolkit.jwt.WPushButton;
 import eu.webtoolkit.jwt.WText;
 
 public class NgsModuleForm extends FormTemplate {
 	private File aaFile = null;
-
+	private FileUpload aaUpload = new FileUpload();
+	private WText infoT = new WText();
+	
 	public NgsModuleForm(final File workDir) {
 		super(tr("admin.config.nsg-module"));
 
 		final WPushButton createDb = new WPushButton("Create NGS database");
 		createDb.disable();
-
-		final FileUpload aaUpload = new FileUpload();
-		final WText infoT = new WText();
 
 		aaUpload.setInline(true);
 
@@ -66,62 +67,82 @@ public class NgsModuleForm extends FormTemplate {
 
 		createDb.clicked().addListener(createDb, new Signal.Listener() {
 			public void trigger() {
-				new File(workDir, Config.TRANSIENT_DATABASES_FOLDER_NAME).mkdirs();
-
-				File unirefVirusesAA50 = new File(workDir, NgsModule.NGS_MODULE_AA_VIRUSES_FASTA);
-
-				String[] args = new String[3];
-				args[0] = aaFile.getAbsolutePath();
-				args[1] = RegaSystemFiles.taxonomyFile().getAbsolutePath();
-				args[2] = unirefVirusesAA50.getAbsolutePath(); // out
-
-				try {
-					new PythonEnv().execPython("/rega/genotype/python/number_fasta_r.py", args);
-				} catch (Exception e1) {
-					e1.printStackTrace();
-					return;
-				}
-
-				// ./diamond makedb --in uniref-viruses-aa50.fa -d uniref-viruses-aa50
-
-				Process p = null;
-
-				File aaVirusesDb = new File(workDir, NgsModule.NGS_MODULE_AA_VIRUSES_DB);
-				String diamondPath = Settings.getInstance().getConfig().getGeneralConfig().getDiamondPath();
-				String cmd = diamondPath + " makedb --in " + unirefVirusesAA50.getAbsolutePath() 
-						+ " -d " + aaVirusesDb.getAbsolutePath();
-
-				System.err.println(cmd);
-				
-				try {
-					p = StreamReaderRuntime.exec(cmd, null, unirefVirusesAA50.getParentFile());
-					int exitResult = p.waitFor();
-
-					if (exitResult != 0) {
-						infoT.setText("Error: could not create " + NgsModule.NGS_MODULE_AA_VIRUSES_DB);
-						return;
-					}
-				} catch (IOException e) {
-					infoT.setText("Error: could not create " + NgsModule.NGS_MODULE_AA_VIRUSES_DB + ". " + e.getMessage());
-					return;
-				} catch (InterruptedException e) {
-					if (p != null)
-						p.destroy();
-					infoT.setText("Error: could not create " + NgsModule.NGS_MODULE_AA_VIRUSES_DB + ". " + e.getMessage());
-					return;
-				}
-
-				NgsModule ngsModule = NgsModule.read(workDir);
-				if (ngsModule == null)
-					ngsModule = new NgsModule();
-				if (!aaUpload.getWFileUpload().isEmpty())
-					ngsModule.setAaFileName(aaUpload.getWFileUpload().getClientFileName());
-				ngsModule.save(workDir);
-
-				infoT.setText("Dimond database created.");
-				dirtyHandler.increaseDirty();
+				createNgsDatabases(workDir);
 			}
 		});
+
+		if (!transientDir(workDir).exists() && isReady()) {
+			StandardDialog d = new StandardDialog("Create NGS databases");
+			d.getContents().addWidget(new WText("NGS databases are not created. Create it now?"));
+			d.finished().addListener(d, new Signal1.Listener<DialogCode>() {
+				public void trigger(DialogCode arg) {
+					if (arg == DialogCode.Accepted)
+						createNgsDatabases(workDir);
+				}
+			});
+		}
+	}
+
+	private File transientDir(File workDir) {
+		return new File(workDir, Config.TRANSIENT_DATABASES_FOLDER_NAME);
+
+	}
+	public void createNgsDatabases(File workDir) {
+		transientDir(workDir).mkdirs();
+		new File(workDir, Config.TRANSIENT_DATABASES_FOLDER_NAME).mkdirs();
+
+		File unirefVirusesAA50 = new File(workDir, NgsModule.NGS_MODULE_AA_VIRUSES_FASTA);
+
+		String[] args = new String[3];
+		args[0] = aaFile.getAbsolutePath();
+		args[1] = RegaSystemFiles.taxonomyFile().getAbsolutePath();
+		args[2] = unirefVirusesAA50.getAbsolutePath(); // out
+
+		try {
+			new PythonEnv().execPython("/rega/genotype/python/number_fasta_r.py", args);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			return;
+		}
+
+		// ./diamond makedb --in uniref-viruses-aa50.fa -d uniref-viruses-aa50
+
+		Process p = null;
+
+		File aaVirusesDb = new File(workDir, NgsModule.NGS_MODULE_AA_VIRUSES_DB);
+		String diamondPath = Settings.getInstance().getConfig().getGeneralConfig().getDiamondPath();
+		String cmd = diamondPath + " makedb --in " + unirefVirusesAA50.getAbsolutePath() 
+				+ " -d " + aaVirusesDb.getAbsolutePath();
+
+		System.err.println(cmd);
+		
+		try {
+			p = StreamReaderRuntime.exec(cmd, null, unirefVirusesAA50.getParentFile());
+			int exitResult = p.waitFor();
+
+			if (exitResult != 0) {
+				infoT.setText("Error: could not create " + NgsModule.NGS_MODULE_AA_VIRUSES_DB);
+				return;
+			}
+		} catch (IOException e) {
+			infoT.setText("Error: could not create " + NgsModule.NGS_MODULE_AA_VIRUSES_DB + ". " + e.getMessage());
+			return;
+		} catch (InterruptedException e) {
+			if (p != null)
+				p.destroy();
+			infoT.setText("Error: could not create " + NgsModule.NGS_MODULE_AA_VIRUSES_DB + ". " + e.getMessage());
+			return;
+		}
+
+		NgsModule ngsModule = NgsModule.read(workDir);
+		if (ngsModule == null)
+			ngsModule = new NgsModule();
+		if (!aaUpload.getWFileUpload().isEmpty())
+			ngsModule.setAaFileName(aaUpload.getWFileUpload().getClientFileName());
+		ngsModule.save(workDir);
+
+		infoT.setText("Dimond database created.");
+		dirtyHandler.increaseDirty();
 	}
 
 	private boolean isReady() { 
